822cd399fa4138b7c846f0c57b52b0a845fa7571
Otavio-burato <otavioburato42@gmail.com>
2024-05-11 23:09:38 -0300

    ajuste no readme e no dev container

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ft_transcendence
 42 network project ft_transcendence
+
+pip install psycopg2-binary
+
+To install Poetry and run it with Django, you can follow these steps:
+
+Install Poetry by running the following command in your terminal:
+
+poetry install
+
+poetry shell
+
+python ./ft_transcendence/manage.py runserver
--- ft_transcendence/ft_transcendence/settings.py
+++ ft_transcendence/ft_transcendence/settings.py
@@ -80,6 +80,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
