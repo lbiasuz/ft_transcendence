15a8e90c981eeb9509f76a54dcbae3aba7332a5f
Otavio-burato <otavioburato42@gmail.com>
2024-07-05 17:47:13 -0300

    Ambiente ELK de pé

--- ft_transcendence/ft_transcendence/core/views.py
+++ ft_transcendence/ft_transcendence/core/views.py
@@ -2,6 +2,11 @@
 # Create your views here.
 
 from django.views.generic import TemplateView
+# import the logging library
+import logging
+
+logger = logging.getLogger(__name__)
 
 class HomeView(TemplateView) : 
     template_name = "index.html"
+    logger.error('Something went wrong!')
