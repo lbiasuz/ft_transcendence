a0492b2104cc613a9495bf3e93305b2b589e63ba
lbiasuz <biasuz.l.leonardo@gmail.com>
2024-07-04 22:55:02 -0300

    fix: set default string for settings

--- ft_transcendence/ft_transcendence/settings.py
+++ ft_transcendence/ft_transcendence/settings.py
@@ -17,12 +17,12 @@ from dj_database_url import parse as dburl
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default=[], cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default='[]', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
