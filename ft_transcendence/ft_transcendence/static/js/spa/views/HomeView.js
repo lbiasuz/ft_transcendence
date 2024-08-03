30f2a8f6d982e0d32a9f4ebc94a63d5e12794294
Otavio-burato <otavioburato42@gmail.com>
2024-08-02 22:38:40 -0300

    teste com sessão?

--- ft_transcendence/ft_transcendence/static/js/spa/views/HomeView.js
+++ ft_transcendence/ft_transcendence/static/js/spa/views/HomeView.js
@@ -23,8 +23,11 @@ export default class HomeView extends View {
 		loginButton.addClass("btn-login");
 
 		loginButton.action(() => {
-			window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6f60df1067aebb00d90db164bd83b278dbf2aacef540c58a5ba333fe86119504&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fsso&response_type=code"
-			Router.navegateTo("/pong-mode");
+			fetch("/ping").then((response) => {
+				let href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6f60df1067aebb00d90db164bd83b278dbf2aacef540c58a5ba333fe86119504&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fsso&response_type=code"
+				if (response.status !== 200) window.location.href = href
+				else Router.navegateTo("/pong-mode");
+			})
 		});
 
 		const footer = new FooterComponent();
