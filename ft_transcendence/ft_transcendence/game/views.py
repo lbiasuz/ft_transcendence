90c27888055673312e3689ceb4c8d1dd6bc001b6
lbiasuz <biasuz.l.leonardo@gmail.com>
2024-08-13 22:10:07 -0300

    fix: filter tournament by user

--- ft_transcendence/ft_transcendence/game/views.py
+++ ft_transcendence/ft_transcendence/game/views.py
@@ -6,10 +6,13 @@ from ft_transcendence.game.models import Match
 
 class MatchViewSet(ModelViewSet):
   serializer_class = MatchSerializer
-  queryset = Match.objects.all().order_by('id')
+  queryset = Match.objects.all()
   filterset_fields = ['game', 'state', 'kind', 'tournament_uuid']
   search_fields = ['uuid', 'session', 'tournament_uuid', 'next_match']
 
+  def get_queryset(self):
+    return super().get_queryset().filter(session=self.request.user).order_by('id')
+  
   def perform_destroy(self, instance: Match):
     instance.state='canceled'
     instance.save()
